5822499aaa9f5266bd5dca3cbb054c0825a6041b
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-20 12:32:36 -0800

    Add all the data associated with a webhook notification event

--- webhook_function/index.js
+++ webhook_function/index.js
@@ -1,8 +1,16 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log('This is where it all starts this is where it gets good');
-    context.log('some data: ' + context.bindings.req.version);
-    context.log('req data: ' + req.body.context.resourceGroupName)
+    context.log('Status: ' + req.body.context.status)
+    context.log('Operation: ' + req.body.context.operation)
+    context.log('Context: ' + req.body.context.context)
+    context.log('Timestamp: ' + req.body.context.timestamp)
+    context.log('ID: ' + req.body.context.id)
+    context.log('Name: ' + req.body.context.name)
+    context.log('SubscriptionID: ' + req.body.context.subscriptionId)
+    context.log('Resource Group Name: ' + req.body.context.resourceGroupName)
+    context.log('Resource Name: ' + req.body.context.resourceName)
+    context.log('Resource type: ' + req.body.context.resourceType)
+    context.log('Resource ID: ' + req.body.context.resourceId)
     context.log('Details: ' + req.body.context.details)
     context.log('Old Capacity' + req.body.context.oldCapacity)
     context.log('New Capacity' + req.body.context.newCapacity)
