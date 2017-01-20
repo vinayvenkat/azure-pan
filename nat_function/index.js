ccd07aab9ae0bc8f6011ecf004689c7fa0cbe41f
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-19 23:49:38 -0800

    Slight modification to the way the queue is accessed

--- nat_function/index.js
+++ nat_function/index.js
@@ -1,6 +1,6 @@
 module.exports = function (context, myTimer, myQueue) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log('This is where it all starts this is where it gets good');
-    context.log('Node.js queue trigger function processed work item', context.bindings.myQueue);
+    context.log('Node.js queue trigger function processed work item', myQueue);
     context.done();
 };
