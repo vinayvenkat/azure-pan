565dcf4746efbab3428b9e9530b0596a81ca76ba
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-20 10:13:30 -0800

    Enable the webhook trigger messages

--- webhook_function/index.js
+++ webhook_function/index.js
@@ -2,12 +2,11 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log('This is where it all starts this is where it gets good');
     context.log('some data: ' + context.bindings.req.version);
-    //context.log('req data: ' + req.body.context.resourceGroupName)
-    //context.log('Details: ' + req.body.context.details)
-    //context.log('Old Capacity' + req.body.context.oldCapacity)
-    //context.log('New Capacity' + req.body.context.newCapacity)
-    //var message = 'Webhook notification message/' + req.body.context.oldCapacity + '/' + req.body.context.newCapacity;
-    var message = 'Webhook notification message/1/2'; 
+    context.log('req data: ' + req.body.context.resourceGroupName)
+    context.log('Details: ' + req.body.context.details)
+    context.log('Old Capacity' + req.body.context.oldCapacity)
+    context.log('New Capacity' + req.body.context.newCapacity)
+    var message = 'Webhook notification message/' + req.body.context.oldCapacity + '/' + req.body.context.newCapacity;
     context.bindings.myQueue = message;
     context.done();
 };
