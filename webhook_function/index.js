dcd9f7f79b17a1ccd5a0550c41bb18164c3c4924
Vinay Venkataraghavan <vvenkatara@paloaltonetworks.com>
2017-01-19 23:07:01 -0800

    Fix syntax errors

--- webhook_function/index.js
+++ webhook_function/index.js
@@ -1,13 +1,13 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log('This is where it all starts this is where it gets good');
-    context.log('some data: ' + context.bindings.req.version)
+    context.log('some data: ' + context.bindings.req.version);
     //context.log('req data: ' + req.body.context.resourceGroupName)
     //context.log('Details: ' + req.body.context.details)
     //context.log('Old Capacity' + req.body.context.oldCapacity)
     //context.log('New Capacity' + req.body.context.newCapacity)
     //var message = 'Webhook notification message/' + req.body.context.oldCapacity + '/' + req.body.context.newCapacity;
-    var message = 'Webhook notification message/1/2' 
+    var message = 'Webhook notification message/1/2'; 
     context.bindings.myQueue = message;
     context.done();
 };
